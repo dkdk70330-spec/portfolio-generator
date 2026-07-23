6d344a192a3e75558fc9bbc987f51c986eb63055
dkdk70330-spec <dkdk70330@gmail.com>
2026-07-23 15:07:33 +0900

    Revert "."
    This reverts commit c103ca0fd34646d64f149e497da7b92f575e0726.

--- template/assets/admin-catalog.js
+++ template/assets/admin-catalog.js
@@ -1,65 +1,51 @@
-(() => {
-  "use strict";
-
-  function deepFreeze(value) {
-    if (!value || typeof value !== "object" || Object.isFrozen(value)) return value;
-    Object.freeze(value);
-    Object.values(value).forEach(deepFreeze);
-    return value;
-  }
-
-  window.ADMIN_CATALOG = deepFreeze({
-    profileLinkServices: [
-      {
-        id: "twitter",
-        name: "X / Twitter",
-        icon: "profile-links/twitter.png"
-      },
-      {
-        id: "instagram",
-        name: "Instagram",
-        icon: "profile-links/instagram.png"
-      },
-      {
-        id: "notice",
-        name: "공지 채널",
-        icon: "profile-links/notice.png"
-      }
-    ],
-
-    platforms: [
-      {
-        id: "bloom",
-        name: "블룸",
-        icon: "platforms/bloom.png"
-      },
-      {
-        id: "caveduck",
-        name: "케이브덕",
-        icon: "platforms/caveduck.png"
-      },
-      {
-        id: "rofan",
-        name: "로판",
-        icon: "platforms/rofan.png"
-      },
-      {
-        id: "tingle",
-        name: "팅글",
-        icon: "platforms/tingle.png"
-      }
-    ],
-
-    genres: [
-      "판타지",
-      "현대",
-      "로맨스",
-      "미스터리",
-      "SF",
-      "드라마",
-      "고딕",
-      "코미디",
-      "정치극"
-    ]
-  });
-})();
+window.ADMIN_CATALOG = {
+  "profileLinkServices": [
+    {
+      "id": "x",
+      "name": "X",
+      "icon": "profile-links/x.png"
+    },
+    {
+      "id": "insta",
+      "name": "Instagram",
+      "icon": "profile-links/insta.png"
+    },
+    {
+      "id": "slash",
+      "name": "슬래시페이지",
+      "icon": "profile-links/slash.png"
+    },
+{
+      "id": "lover",
+      "name": "러버미",
+      "icon": "profile-links/lover.png"
+    }
+  ],
+  
+"platforms": [
+    {
+      "id": "bloom",
+      "name": "블룸",
+      "icon": "platforms/bloom.png"
+    },
+    {
+      "id": "caveduck",
+      "name": "케이브덕",
+      "icon": "platforms/caveduck.png"
+    },
+    {
+      "id": "rofan",
+      "name": "로판",
+      "icon": "platforms/rofan.png"
+    },
+    {
+      "id": "tingle",
+      "name": "팅글",
+      "icon": "platforms/tingle.png"
+    }
+  ],
+        "genres": [
+        "판타지",
+        "미스터리"
+      ]
+};
